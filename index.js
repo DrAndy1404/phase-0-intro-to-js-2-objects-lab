8dc280caee437691c9435addadb3d40a4967fb31
Andrew <andrew.dimou10@gmail.com>
2024-03-13 14:43:22 +1100

    Initialize git

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const updatedEmployee = { ...employee };
+  updatedEmployee[key] = value;
+  return updatedEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  const updatedEmployee = { ...employee };
+  delete updatedEmployee[key];
+  return updatedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
+
+const employee = {
+  name: "Andrew",
+  age: 30,
+  streetAddress: "123 Small Street",
+};
+
+updateEmployeeWithKeyAndValue(employee, "age", 35);
+destructivelyUpdateEmployeeWithKeyAndValue(employee, "name", "John");
+deleteFromEmployeeByKey(employee, "streetAddress");
+destructivelyDeleteFromEmployeeByKey(employee, "age");
